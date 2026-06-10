0cdb60cc2d0f031d0c45d8be6607a7a314085e3f
BrunoMatta <bruno_11_02@live.com>
2026-06-09 23:24:13 -0500

    ms-pedidos 1er push

--- ms-pedidos/src/main/java/com/cafeteria/pedidos/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cafeteria.pedidos.repository;
-
-import com.cafeteria.pedidos.entity.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-    List<Pedido> findByTurnoId(Long turnoId);
-}
