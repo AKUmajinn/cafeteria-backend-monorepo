5e63c633eef874c106ae734d61513d942ac8f354
BrunoMatta <bruno_11_02@live.com>
2026-06-12 00:03:35 -0500

    2do push

--- ms-pedidos/src/main/java/com/cafeteria/pedidos/repository/TurnoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cafeteria.pedidos.repository;
-
-import com.cafeteria.pedidos.entity.Turno;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface TurnoRepository extends JpaRepository<Turno, Long> {
-    Optional<Turno> findByEstado(String estado);
-}
